e544a90c06ffe66c65d4f8c3babb4bff502a54c9
Your Name <you@example.com>
2024-09-03 22:53:29 +0300

    Pet Project. Counter JS. Simple web application.

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React from 'react'
+
+function App() {
+  
+const [count, setCount] = React.useState(0)
+
+  const onClickPlus = () => {
+    setCount(count + 1);
+  }
+  const onClickMinus = () => {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Счётчик: </h2>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">- Минус</button>
+        <button onClick={onClickPlus} className="plus">Плюс +</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
